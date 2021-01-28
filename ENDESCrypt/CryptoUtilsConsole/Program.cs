155121b13d8212b3a82840624308f175b4301081
Burak Dinç <burak.08.44@gmail.com>
2021-01-28 05:33:27 +0300

    ENCrypt-DESCrypt
    String veri tipleri için şifreleme ve şifre çözme işlemleri.

---
+++ ENDESCrypt/CryptoUtilsConsole/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoUtilsConsole
+{
+    public enum CryptoOperation
+    {
+        ENCRYPT,
+        DECRYPT
+    };
+
+    class DESDemos
+    {
+
+        public static byte[] DESCrypto(CryptoOperation cryptoOperation, DESCryptoServiceProvider des, byte[] message)
+        {
+
+            using (var memStream = new MemoryStream())
+            {
+                CryptoStream cryptoStream = null;
+
+                if (cryptoOperation == CryptoOperation.ENCRYPT)
+                    cryptoStream = new CryptoStream(memStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+                else if (cryptoOperation == CryptoOperation.DECRYPT)
+                    cryptoStream = new CryptoStream(memStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+
+                if (cryptoStream == null)
+                    return null;
+
+                cryptoStream.Write(message, 0, message.Length);
+                cryptoStream.FlushFinalBlock();
+                return memStream.ToArray();
+            }
+
+        }
+
+        public static byte[] GenerateRandomByteArray(int size)
+        {
+            var random = new Random();
+            byte[] byteArray = new byte[size];
+            random.NextBytes(byteArray);
+            return byteArray;
+        }
+
+        public static byte[] GenerateIv()
+        {
+            byte[] IV = GenerateRandomByteArray(8);
+            return IV;
+        }
+
+        public static byte[] GenerateKey()
+        {
+            byte[] key = GenerateRandomByteArray(8);
+            return key;
+        }
+
+        public static void LaunchDemo(string metin)
+        {
+            using (var des = new DESCryptoServiceProvider())
+            {
+                des.GenerateIV();
+                des.GenerateKey();
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+
+                byte[] encrypted = DESCrypto(CryptoOperation.ENCRYPT, des, Encoding.UTF8.GetBytes(metin));
+                Console.WriteLine(Environment.NewLine + "Encrypted Text :" + BitConverter.ToString(encrypted).Replace("-", ""));
+                byte[] decrypted = DESCrypto(CryptoOperation.DECRYPT, des, encrypted);
+                Console.WriteLine(Environment.NewLine + "Decrypted Text :" + Encoding.UTF8.GetString(decrypted));
+            }
+            Console.ReadLine();
+        }    
+    }
+
+    class Program
+    {
+        static void Main(string[] args) // -- Ana program burası. Çalıştırdığında ilk buranın içi çalışır.-- 
+        {
+            Console.Write("Şifrelenecek Metni Giriniz: ");
+            string metin = Console.ReadLine(); // (1.) kullanıcıdan şifrelemek için metin girmesi istenir                     
+
+            DESDemos.LaunchDemo(metin); //(2.) AESDemos sınıfından LaunchDemo fonksiyonu çağrılır.
+
+        }
+    }
+}
---
+++ ENDESCrypt/README.md
@@ -0,0 +1,13 @@
+# Introduction
+Csharp project with code snippets on Cryptography using the System.Security.Cryptography API
+provided by Microsoft built-in functions and classes in Csharp.
+It was meant to be used in my playlist of Csharp + Cryptography in Youtube:
+[Melar Dev Youtube Channel](https://youtube.com/Melardev)
+You can also follow me on social media
+- [Twitter](https://twitter.com/melardev) I share a lot of dev/IT/security tips
+- [Blog](http://melardev.com) My personal blog
+- [Instagram](https://www.instagram.com/melar_dev/) Not very active but hey I may post some banners or maybe myself ....
+=======
+# TutsCSharpCrypto
+Youtube Playlist about using Crypto API in C# https://www.youtube.com/playlist?list=PLfkTJXI2Tk-dOnQiSdG29YHnKL5ViEhjt
+Tutoriales de criptografía en C# en Youtube https://www.youtube.com/playlist?list=PLfkTJXI2Tk-dOnQiSdG29YHnKL5ViEhjt
